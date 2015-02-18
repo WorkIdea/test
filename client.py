347c5d7ba83b8b2766462575c2fd32626291f2f7
ImpegI <r.sortane@gmail.com>
2015-02-18 15:32:25 +0200

    Primeiro commit

--- client.py
+++ client.py
@@ -1,2 +1,8 @@
 __author__ = 'ImpegI'
 import socket
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+print("Default socket timeout: %s",s.gettimeout())
+s.timeout(100)
+print("Current socket timeout: %s",s.gettimeout())
