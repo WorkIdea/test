e924b06da48f655b6de45e930c42760e16d41c3c
ImpegI <r.sortane@gmail.com>
2015-02-18 15:41:38 +0200

    Primeiro commit

--- client.py
+++ client.py
@@ -4,5 +4,9 @@ import socket
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 print("Default socket timeout: %s",s.gettimeout())
-s.timeout(100)
+s.settimeout(100)
 print("Current socket timeout: %s",s.gettimeout())
+
+host_name = socket.gethostname()
+
+print("Host Name: %s",host_name)
