35deadca934b34c5067ae11ce6f9b947831f5895
ImpegI <r.sortane@gmail.com>
2015-02-19 17:09:58 +0200

    Primeiro commit

--- client.py
+++ client.py
@@ -11,4 +11,4 @@ host_name = socket.gethostname()
 
 print("Host Name: %s",host_name)
 
-print("IP address: %s",socket.gethostbyname(hostname))
+print("IP address: %s",socket.gethostbyname(host_name))
